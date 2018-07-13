1606b5f25703b39be8580853b1a778d8daceae78
geoffrey woodcock <theatomproject@gmail.com>
2018-07-13 19:20:55 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -19,7 +19,8 @@ class Application
       @@cart.empty?
        resp.write "Your cart is empty."
      else
-       
+       @@cart.each do |item|
+         resp.write "#{item}\n"
      end 
    end
     elsif 
